18db5fa2c88ade6892cd8723ad5226d669630e28
AbubakrShaikh <21390@students.mrgs.school.nz>
2024-05-15 14:48:21 +1200

    Started creating intro frame for program

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from tkinter import *
 
+
 window = Tk()
-window.geometry("300x300")
+window.geometry("500x500")
 window.title("My App")
+
+intro_frame = Frame(background="#8094dc", width=500, height=500)
+intro_frame.grid()
+
+intro_title = Label(background="#8094dc", text="Welcome to...", font=25)
+intro_title.grid(row=0, column=0)
 
 window.mainloop()
