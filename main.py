c649662aa74d07ec16fe2c1d7d1c47cda906802c
AbubakrShaikh <21390@students.mrgs.school.nz>
2024-05-22 14:54:34 +1200

    Add Continue Button to intro frame

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ window.title("My App")
 intro_frame = Frame(background="#8094dc", width=500, height=500)
 intro_frame.grid()
 
-intro_title = Label(background="#8094dc", text="Welcome to...", font=25)
+intro_title = Label(intro_frame, background="#8094dc", text="Welcome to...", font=25)
 intro_title.grid(row=0, column=0)
+
+redirect_button = Button(intro_frame, text="Continue", height=3, width=7)
+redirect_button.grid()
+
 
 window.mainloop()
