a49219de3cc5e693285848b58185857c199e4470
AbubakrShaikh <21390@students.mrgs.school.nz>
2024-05-30 13:29:20 +1200

    Added function for exit wit saftey precations

--- intro_page.py
+++ intro_page.py
@@ -8,18 +8,35 @@ from app_settings import *
 
 class start_page():
     def __init__(self):
-        intro_frame = Frame(bg=bg_color, width=w_width, height=w_height)
-        intro_frame.pack()
+        self.intro_frame = Frame(bg=bg_color, width=w_width, height=w_height)
+        self.intro_frame.pack()
 
-        instruct_label = Label(intro_frame, bg=bg_color, text="Some Placeholder Text", font=head_font)
-        instruct_label.grid(row=0, column=0, pady=220, columnspan=2 )
+        self.instruct_label = Label(self.intro_frame, bg=bg_color, text="Some Placeholder Text", font=head_font)
+        self.instruct_label.grid(row=0, column=0, pady=220, columnspan=2 )
 
-        continue_button = Button(intro_frame, bg="#eeeeff", text="Continue", font=button_font, width=20, height=2)
+        continue_button = Button(self.intro_frame, bg="#eeeeff", text="Continue", font=button_font, width=20, height=2)
         continue_button.grid(row=1, column=0, padx=50, pady=125)
 
-        exit_button = Button(intro_frame, bg="#aaaaff", text="Exit", font=button_font, width=20, height=2, command=)
-        exit_button.grid(row=1, column=1, padx=50, pady=125)
+        self.exit_button = Button(self.intro_frame, bg="#eeeeff", text = "Exit", font=button_font, width=20, height=2, command=self.collect_name)
+        self.exit_button.grid(row=1, column=1, padx=50, pady=125)
 
-    def exit_function():
-        confirm_exit = Tk()
-        confirm_exit.geometry("")
+    #This function collects the user's name then destroys the initial_frame frame
+    def collect_name(self):
+        self.confirm_window = Tk()
+
+        self.yes_no = IntVar()
+
+        self.confirm_question = Label(self.confirm_window, text="Are you sure you want to exit?")
+        self.confirm_question.grid(row=0, column=0, columnspan=2)
+
+        self.confirm_yes = Button(self.confirm_window, text="Yes", command=lambda:[self.confirm_window.destroy(), self.intro_frame.destroy()])
+        self.confirm_yes.grid(row=1, column=0)
+
+        
+
+        self.confirm_no = Button(self.confirm_window, text="No", command=self.confirm_window.destroy())
+        self.confirm_no.grid(row=1, column=1)
+
+        self.confirm_window.mainloop()
+
+
