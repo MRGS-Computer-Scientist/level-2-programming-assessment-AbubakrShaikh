e60a1895ae6dea346d9fe911958c406fca9db4c9
AbubakrShaikh <21390@students.mrgs.school.nz>
2024-05-31 12:35:10 +1200

    Finished Intro frame

--- app_settings.py
+++ app_settings.py
@@ -1,3 +1,30 @@
+from tkinter import *
+
+#Function to exit the app
+def exit_function():
+    confirm_window = Tk()
+    
+    def pressed_yes():
+        confirm_window.destroy()
+        intro_frame.destroy()
+
+    def pressed_no():
+        confirm_window.destroy()
+
+    confirm_question = Label(confirm_window, text="Are you sure you want to exit?")
+    confirm_question.grid(row=0, column=0, columnspan=2)
+
+    confirm_yes = Button(confirm_window, text="Yes", command=pressed_yes)
+    confirm_yes.grid(row=1, column=0)
+
+    confirm_no = Button(confirm_window, text="No", command=pressed_no)
+    confirm_no.grid(row=1, column=1)
+
+    confirm_window.mainloop()
+
+
+
+
 #Variables to be used in code
 w_width = 1600
 w_height = 900
--- intro_page.py
+++ intro_page.py
@@ -2,41 +2,23 @@ from tkinter import *
 from app_settings import *
 
 #This will be what the user sees when the app opens
-
-
-
-
 class start_page():
     def __init__(self):
+        #Create a frame on which the functions will sit
         self.intro_frame = Frame(bg=bg_color, width=w_width, height=w_height)
         self.intro_frame.pack()
 
+        #Label for the title
         self.instruct_label = Label(self.intro_frame, bg=bg_color, text="Some Placeholder Text", font=head_font)
         self.instruct_label.grid(row=0, column=0, pady=220, columnspan=2 )
 
-        continue_button = Button(self.intro_frame, bg="#eeeeff", text="Continue", font=button_font, width=20, height=2)
-        continue_button.grid(row=1, column=0, padx=50, pady=125)
+        #Button to continue onto the program
+        self.continue_button = Button(self.intro_frame, bg="#eeeeff", text="Continue", font=button_font, width=20, height=2)
+        self.continue_button.grid(row=1, column=0, padx=50, pady=125)
 
-        self.exit_button = Button(self.intro_frame, bg="#eeeeff", text = "Exit", font=button_font, width=20, height=2, command=self.collect_name)
+        #Button for exiting the program
+        self.exit_button = Button(self.intro_frame, bg="#eeeeff", text = "Exit", font=button_font, width=20, height=2, command=exit_function)
         self.exit_button.grid(row=1, column=1, padx=50, pady=125)
 
-    #This function collects the user's name then destroys the initial_frame frame
-    def collect_name(self):
-        self.confirm_window = Tk()
-
-        self.yes_no = IntVar()
-
-        self.confirm_question = Label(self.confirm_window, text="Are you sure you want to exit?")
-        self.confirm_question.grid(row=0, column=0, columnspan=2)
-
-        self.confirm_yes = Button(self.confirm_window, text="Yes", command=lambda:[self.confirm_window.destroy(), self.intro_frame.destroy()])
-        self.confirm_yes.grid(row=1, column=0)
-
-        
-
-        self.confirm_no = Button(self.confirm_window, text="No", command=self.confirm_window.destroy())
-        self.confirm_no.grid(row=1, column=1)
-
-        self.confirm_window.mainloop()
-
+    #This function asks the user to confirm then destroys the initial_frame frame (need to move to app_settings.py)
 
